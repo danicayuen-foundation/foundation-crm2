681d649cf1085dfa0806f8cbd8fad1e9bbd49708
danicayuen-foundation <danica.yuen@foundation.bot>
2026-05-20 15:45:10 -0700

    Remove supabase import from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { createRoot } from "react-dom/client";
 import * as XLSX from "xlsx";
-import { supabase } from "./supabase";
 import {
   Plus,
   Download,
